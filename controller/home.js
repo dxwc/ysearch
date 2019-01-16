c191ec9e9c34181199602b9bd73fd012da758b0e
dxwc <dxwc@users.noreply.github.com>
2019-01-15 23:45:25 -0500

    embed and direct link

--- controller/home.js
+++ controller/home.js
@@ -1,5 +1,7 @@
-let router = require('express').Router();
-const ytsr = require('ytsr');
+let router             = require('express').Router();
+const ytsr             = require('ytsr');
+const fetchCommentPage = require('youtube-comment-api');
+const vd               = require('vid_data');
 
 router.post('/', (req, res) =>
 {
@@ -8,6 +10,11 @@ router.post('/', (req, res) =>
     ytsr(req.body.terms, { limit : 20 })
     .then((result) =>
     {
+        result.items.forEach((video) =>
+        {
+            if(video.type === 'video') video.id = vd.get_video_id(video.link);
+        });
+
         return res.json(result.items);
 
         // .forEach((val) =>
@@ -36,5 +43,16 @@ router.get('/', (req, res) =>
 {
     res.render('index');
 });
+
+// const videoId = 'h_tkIpwbsxY'
+
+// fetchCommentPage(videoId)
+//   .then(commentPage => {
+//     console.log(commentPage.comments)
+//     return fetchCommentPage(videoId, commentPage.nextPageToken)
+//   })
+//   .then(commentPage => {
+//     console.log(commentPage.comments)
+//   })
 
 module.exports = router;
